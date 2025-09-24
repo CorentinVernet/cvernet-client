96548d9ef0877a202c78dbe2b895c453b061bb05
Corentin <corentinvernet89@gmail.com>
2025-09-24 19:29:09 +0200

    change style burger

--- js/menu.js
+++ js/menu.js
@@ -4,12 +4,14 @@ const nav = document.getElementById("nav");
 if (burger && nav) {
   burger.addEventListener("click", () => {
     nav.classList.toggle("show");
+    burger.classList.toggle("active"); // <-- ajoute cette ligne
   });
 
   // Ferme le menu quand on clique sur un lien
   document.querySelectorAll(".nav a").forEach((link) => {
     link.addEventListener("click", () => {
       nav.classList.remove("show");
+      burger.classList.remove("active"); // <-- et celle-ci
     });
   });
 }
